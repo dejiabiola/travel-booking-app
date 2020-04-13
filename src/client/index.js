87a0c603257e7e63b9b7555efd272a839c7faf79
Adedeji Abiola <adedejiabiola56@gmail.com>
2020-04-13 04:32:24 +0100

    code cleanup

--- src/client/index.js
+++ src/client/index.js
@@ -3,14 +3,16 @@ import { init } from './js/app';
 
 
 
-// Import 
+// Import main css
 import './styles/entry.scss';
 
+// Inport images
 import './media/sun.svg';
 import './media/aircraft.svg';
 import './media/calendar.svg';
 import './media/time.svg';
 import './media/25.svg';
+import './media/background1.jpg'
 
 
 window.addEventListener("DOMContentLoaded", init);
